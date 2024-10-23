90a6a094166922809cf889ee3f1d7dc1c24e3b89
Jaden <jadenchen95@yahoo.ca>
2024-10-23 00:28:40 -0400

    internal project initial

---
+++ InternalProject/InternalProject/app/src/main/java/com/example/jerry/internalproject/Matching.java
@@ -0,0 +1,13 @@
+package com.example.jerry.internalproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Matching extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_matching);
+    }
+}
